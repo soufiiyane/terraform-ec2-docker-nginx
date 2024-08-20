21459e0f666ff8e43324b512e6e558aae137cd5e
soufiiyane <soufiyane.aitmoulay@gmail.com>
2024-08-20 17:30:43 +0100

    s3 back-end state

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "my-bucket"
+    key = "my-terraform-state"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
